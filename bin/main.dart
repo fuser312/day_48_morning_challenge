9d226093df4fdbb5086c9aad729f64b8ca0d9a7f
Sahil Singh <sahil.dogstar@gmail.com>
2019-12-10 10:25:51 +0530

    First part of the problem solved.

--- bin/main.dart
+++ bin/main.dart
@@ -1,4 +1,4 @@
-class Student{
+class Student {
   String name;
   int score;
 
@@ -17,7 +17,38 @@ class Student{
 
 // Challenge 4 - Sort the students based on their scores
 
-
 void main() {
+  Student first = Student('Ram', 87);
+  Student second = Student('Shyam', 80);
+  Student third = Student('Ghanshyam', 91);
 
+  List<Student> students = [first, second, third];
+  for (Student item in students) {
+    print("${item.name} got ${item.score}");
+  }
+  Student best = students.reduce(bestStudent);
+  print("${best.name} is the best student with a score of ${best.score}");
+
+  students.sort(compareStudentScore);
+  for (Student item in students) {
+    print("${item.name} got ${item.score}");
+  }
+}
+
+Student bestStudent(Student one, Student two) {
+  if (one.score < two.score) {
+    return two;
+  } else {
+    return one;
+  }
+}
+
+int compareStudentScore(Student student1, Student student2) {
+  if (student1.score < student2.score) {
+    return -1;
+  } else if (student1.score == student2.score) {
+    return 0;
+  } else {
+    return 1;
+  }
 }
--- test/day_48_morning_challenge_test.dart
+++ test/day_48_morning_challenge_test.dart
@@ -1,4 +1,5 @@
 import 'package:test/test.dart';
+import '../bin/main.dart';
 
 void main() {
   test('Test Case', () {
